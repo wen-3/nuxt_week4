033cfab8380266e929197ff5e01281850bc52930
wen-3 <sd63324@gmail.com>
2024-12-02 21:33:02 +0800

    Pinia 改用 Composition API 寫法

--- stores/index.js
+++ stores/index.js
@@ -1,16 +1,34 @@
 import { defineStore } from "pinia";
 
-export const useAddressStore = defineStore("address", {
- state: () => ({ 
-    count: 0,
-    name: "Mike",
-}), 
- getters: {
-   double: (state) => state.count * 2,
- },
- actions: {
-   addCount() {
-     this.count++;
-   },
- },
+export const useAddressStore = defineStore("address", () => {
+    const count = ref(0);
+    const name = ref('Mike');
+
+    const double = computed(() => count.value * 2);
+
+    const addCount = () => {
+        count.value += 1;
+    };
+
+    return{
+        count,
+        name,
+        double,
+        addCount
+    };
 });
+
+// export const useAddressStore = defineStore("address", {
+//  state: () => ({ 
+//     count: 0,
+//     name: "Mike",
+// }), 
+//  getters: {
+//    double: (state) => state.count * 2,
+//  },
+//  actions: {
+//    addCount() {
+//      this.count++;
+//    },
+//  },
+// });
